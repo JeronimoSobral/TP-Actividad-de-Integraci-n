7e9749e771ba4143b47f5d9cd804a1f4fad48745
JeronimoSobral <116697514+JeronimoSobral@users.noreply.github.com>
2023-03-20 20:55:35 -0300

    Add files via upload

--- Integrador1-8/6.py
+++ Integrador1-8/6.py
@@ -4,9 +4,18 @@ class Persona:
          self.edad = edad
          self.dni = dni 
         
-def get_name(self):
+def get_nombre(self):
         return self.nombre
-person1 = Persona("John", 48,43184396)
+def get_edad(self):
+        return self.edad
+def get_dni(self):
+        return self.dni
+def set_nombre(self, nombre):
+        self.nombre = nombre
+def set_dni(self, dni):
+        self.dni = dni
+def set_edad(self, edad):
+        self.edad = edad
+person1 = Persona("Carlitos", 48,43184396)
 person2 = Persona("Jero", 21,43184396)
 person3 = Persona("Flor", 22,43184396)
-print  (get_name(person1))
